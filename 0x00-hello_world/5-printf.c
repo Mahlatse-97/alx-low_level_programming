9374ce1cd2f44ec88f9d4ded48eaeedb482529ce
Mahlatse-97 <sebopamahlatsee@gmail.com>
2023-06-18 11:32:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.hi>
+/**
+*main - A programe that prints a line using a printf function
+*Return: o (success)
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (o);
+}
